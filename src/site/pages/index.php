79a77277a1760811b8a3d5aa484ab44f610b13a6
po-ptitjo <joseph.tramier1@gmail.com>
2025-11-25 13:40:32 +0100

    ajout des fragments du header et de la navbar au fichier index.php

--- src/site/pages/index.php
+++ src/site/pages/index.php
@@ -1,36 +1,9 @@
 <?php
-
+include("../fragment/header.html");
+include("../fragment/navbar.html");
 ?>
 
-<!doctype html>
-<html lang="fr">
-
-  <head>
-    <meta charset="utf-8">
-    <title>SAE_Maquette</title>
-    <link rel="stylesheet" href="../css/style.css">
-  </head>
-
   <body>
-    <header>
-      <div class="nav-container">
-        <nav>
-          <ul class="nav-menu nav-left">
-            <li><a href="index.php" class="bouton-nav">Accueil</a></li>
-            <li><a href="tech.php" class="bouton-nav">Technicien</a></li>
-          </ul>
-        </nav>
-
-        <div class="site-name">Nom_du_site</div>
-
-        <nav>
-          <ul class="nav-menu">
-            <li><a href="connexion.php" class="bouton-nav" id="bouton-connexion">Connexion</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-
     <div class="main-content">
       <h1>Bienvenue</h1>
       <p class="welcome-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam culpa eaque ipsa iusto quae quos repellat saepe sapiente similique velit. Consectetur incidunt itaque neque odit!</p>
