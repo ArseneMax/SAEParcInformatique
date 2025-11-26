048aefe68fdc01786dcaf1eb97b32b5ac65642e0
po-ptitjo <joseph.tramier1@gmail.com>
2025-11-26 11:10:12 +0100

    ajout de l'action au formulaire de connexion et de la déconnexion

---
+++ src/site/pages/actions/actionConnexion.php
@@ -0,0 +1,26 @@
+<?php
+$nom_table = "sae";
+
+$connect = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($connect,$nom_table);
+
+if (isset($_POST['login'],$_POST['password'],$_POST['connexion'])) {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    if ($login !="" && $password !="") {
+        $select = "SELECT * FROM users WHERE login = '$login' and password = '$password'";
+        $requete_log = mysqli_query($connect,$select);
+        if (mysqli_num_rows($requete_log) == 1 ) {
+            session_start();
+            $_SESSION['login'] = $login;
+            $_SESSION['password'] = $password;
+            header("location:../index.php");
+        }
+        else{
+            echo "error";
+            header("location:../connexion.php?error");
+        }
+    }else{
+        header('location:../connexion.php?error');
+    }
+}
