aea19828362b3a09e22e64a6427996731f61198f
po-ptitjo <joseph.tramier1@gmail.com>
2025-12-01 14:27:40 +0100

    ajout page admin dans la bar de navigation si c'est l'adminweb qui est connecté

--- src/site/fragment/navbar.php
+++ src/site/fragment/navbar.php
@@ -1,24 +1,31 @@
-<?php
-echo"<header>
+<header>
     <div class='nav-container'>
         <nav>
             <ul class='nav-menu nav-left'>
                 <li><a href='index.php' class='bouton-nav'>Accueil</a></li>
                 <li><a href='tech.php' class='bouton-nav'>Technicien</a></li>
+                <?php
+                session_start();
+                if (isset($_SESSION['login'])) {
+                    if ($_SESSION['login'] == 'adminweb') {
+                        echo"<li><a href='adminweb.php' class='bouton-nav'>Admin</a></li>";
+                    }
+                }
+                 ?>
+
             </ul>
         </nav>
 
         <div class='site-name'>ParkIT</div>
 <nav>
-        <ul class='nav-menu'>";
-            session_start();
+        <ul class='nav-menu'>
+            <?php
             if (isset($_SESSION['login'])) {
             echo "<li><a href='logout.php' class='bouton-nav' id='bouton-connexion'> Déconnexion </a></li>";}
             else {
                 echo "<li><a href='connexion.php' class='bouton-nav' id='bouton-connexion'>Connexion</a></li>";
-            }
-            echo"</ul>
+            }?>
+            </ul>
     </nav>
     </div>
-</header>";
-?>
+</header>
