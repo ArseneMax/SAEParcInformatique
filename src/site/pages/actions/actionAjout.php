bec33cff73d7efb58d6d235969369bb57baab471
po-ptitjo <joseph.tramier1@gmail.com>
2025-12-01 14:29:43 +0100

    creation de la page adminweb avec un formulaire de creation de compte tecnicien et de sa page action

---
+++ src/site/pages/actions/actionAjout.php
@@ -0,0 +1,31 @@
+<?php
+$nom_BD = "parkit";
+
+$connect = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($connect, $nom_BD);
+
+if (isset($_POST['login'], $_POST['password'], $_POST['creation'])) {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $role = "tech";
+
+    if ($login != "" && $password != "") {
+        $select = "SELECT * FROM users WHERE login = '$login'";
+        $requete_log = mysqli_query($connect,$select);
+        if (mysqli_num_rows($requete_log) < 1 ) {
+            $insert = "INSERT INTO users VALUES (?,?,?)";
+            $requete = mysqli_prepare($connect, $insert);
+            mysqli_stmt_bind_param($requete, "sss", $login, $password,$role);
+            mysqli_stmt_execute($requete);
+            mysqli_stmt_close($requete);
+            header('location:../adminweb.php');
+        }
+        else{
+
+            header('location:../adminweb.php?error');
+        }
+
+    } else {
+        header('location:../adminweb.php?error');
+    }
+}
---
+++ src/site/pages/adminweb.php
@@ -0,0 +1,21 @@
+<?php
+include("../fragment/header.html");
+include("../fragment/navbar.php");
+?>
+<div class='connexion-page'>
+    <div class='form-container'>
+        <h1 class='form-title'>Créer un technicien</h1>
+        <form method='post' action='actions/actionAjout.php'>
+            <?php include("../fragment/formConnexion.html"); ?>
+            <button type="submit" class="form-button" name="creation">Créer</button>
+        </form>
+        <?php
+        if (isset($_GET['error'])){
+            echo"<div class='connexion-error'>
+            Identifiant existant !
+    </div>";
+        }
+        ?>
+    </div>
+</div>
+</body>
