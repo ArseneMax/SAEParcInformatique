db0c99d65ef9885c7f811f688f92187fc68d95b0
Arsene Maxence <arsenemaxencezs@gmail.com>
2025-12-05 12:26:04 +0100

    déplacement des fichiers + réarrengement de la navbar (refaire l'action car bizarre)

--- src/site/fragment/navbarTech.php
+++ src/site/fragment/navbarTech.php
@@ -4,6 +4,7 @@
             <ul class='sub-nav-menu sub-nav-left'>
                 <li><a href='ordinateur.php' class='sub-bouton-nav'>Ordinateur</a></li>
                 <li><a href='moniteur.php' class='sub-bouton-nav'>Moniteur</a></li>
+                <li><a href='modifierMachine.php' class='sub-bouton-nav'>Modifier Machine</a></li>
             </ul>
         </nav>
     </div>
