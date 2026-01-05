7297b94a1bc3b2ca6196019b7cc29703e6f5d254
po-ptitjo <joseph.tramier1@gmail.com>
2026-01-05 15:22:56 +0100

    Action de la fonction insertionLog

---
+++ src/site/fonctions/fonctionsLog.php
@@ -0,0 +1,16 @@
+<?php
+include("database.php");
+
+function insertionLog($action){
+    $insertLog = "INSERT INTO journal (login,ip,role,action,date,heure) VALUES (?,?,?,?,?,?)";
+    $stmt2 = mysqli_prepare($connect,$insertLog);
+    session_start();
+    $login = $_POST['login'];
+    $role = $_SESSION['role'];
+    $date = date("Y-m-d");
+    $ip =  $_SERVER['REMOTE_ADDR'];
+    $heure = date("H:i:s");
+    mysqli_stmt_bind_param($stmt2,"ssssss",$login,$ip,$role,$action,$date,$heure);
+    mysqli_stmt_execute($stmt2);
+    mysqli_stmt_close($stmt2);
+}
