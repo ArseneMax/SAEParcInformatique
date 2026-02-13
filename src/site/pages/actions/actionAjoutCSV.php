2856de8391bdbc5602966521ae99ec2821aa10ef
po-ptitjo <joseph.tramier1@gmail.com>
2026-02-13 11:10:08 +0100

    AjoutCSV fonctionnel et gestion de l'affichage des erreurs

--- src/site/pages/actions/actionAjoutCSV.php
+++ src/site/pages/actions/actionAjoutCSV.php
@@ -6,113 +6,112 @@ if (isset($_POST['submit']) && isset($_POST['type_csv'])) {
     if (isset($_FILES['csv_file']) && $_FILES['csv_file']['error'] == 0) {
         $file = $_FILES['csv_file']['tmp_name'];
 
+        $statut = 'actif';
+
         if (($handle = fopen($file, 'r')) !== FALSE) {
-            fgetcsv($handle);
+            $header = fgetcsv($handle); // Lecture de la première ligne (entêtes)
 
+            // Définir les colonnes requises en fonction du type sélectionné
             if ($_POST['type_csv'] == "moniteurs") {
-                $insert = "INSERT INTO moniteur (SERIAL, MANUFACTURER, MODEL, SIZE_INCH, RESOLUTION, CONNECTOR, ATTACHED_TO) VALUES (?,?,?,?,?,?,?)";
+                $requiredColumns = ['SERIAL'];
+                $insertQuery = "INSERT INTO moniteur (SERIAL, MANUFACTURER, MODEL, SIZE_INCH, RESOLUTION, CONNECTOR, ATTACHED_TO,statut) VALUES (?, ?, ?, ?, ?, ?, ?,?)";
                 $checkSerialQuery = "SELECT COUNT(*) FROM moniteur WHERE SERIAL = ?";
-
-                if (!$connect) {
-                    die("Connection failed: " . mysqli_connect_error());
-                }
                 $action = "Import de moniteurs via CSV";
-
-                if ($stmt = mysqli_prepare($connect, $insert)) {
-                    if ($checkStmt = mysqli_prepare($connect, $checkSerialQuery)) {
-                        $errorOccurred = false;
-
-                        while (($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
-                            if (count($data) == 7) {
-                                mysqli_stmt_bind_param($checkStmt, 's', $data[0]);
-
-                                mysqli_stmt_execute($checkStmt);
-
-                                mysqli_stmt_bind_result($checkStmt, $count);
-                                mysqli_stmt_fetch($checkStmt);
-
-                                mysqli_stmt_free_result($checkStmt);
-
-                                if ($count > 0) {
-                                    $errorOccurred = true;
-                                } else {
-                                    mysqli_stmt_bind_param($stmt, 'sssisss', $data[0], $data[1], $data[2], $data[3], $data[4], $data[5], $data[6]);
-                                    mysqli_stmt_execute($stmt);
-                                }
-                            } else {
-                                header("Location: ../ajoutCSVMachines.php?error=invalid_columns");
-                                exit();
-                            }
-                        }
-
-                        if ($errorOccurred) {
-                            header("Location: ../ajoutCSVMachines.php?error=serial_exists");
-                            exit();
-                        }
-                        insertionLog($action);
-
-                        header("Location: ../ajoutCSVMachines.php?sucess");
-                        exit();
-                    } else {
-                        die('Error preparing check serial query: ' . mysqli_error($connect));
-                    }
-                } else {
-                    die('Error preparing insert query: ' . mysqli_error($connect));
-                }
+            } elseif ($_POST['type_csv'] == "ordinateurs") {
+                $requiredColumns = ['NAME', 'SERIAL'];
+                $insertQuery = "INSERT INTO ordinateur (NAME, SERIAL, MANUFACTURER, MODEL, TYPE, CPU, RAM_MB, DISK_GB, OS, DOMAIN, LOCATION, BUILDING, ROOM, MACADDR, PURCHASE_DATE, WARRANTY_END, statut) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                $checkSerialQuery = "SELECT COUNT(*) FROM ordinateur WHERE NAME = ?";
+                $action = "Import d'ordinateurs via CSV";
+            } else {
+                header("Location: ../ajoutCSVMachines.php?error=invalid_type");
+                exit();
             }
 
-            if ($_POST['type_csv'] == "ordinateurs") {
-                $insert = "INSERT INTO ordinateur (NAME, SERIAL, MANUFACTURER, MODEL, TYPE, CPU, RAM_MB, DISK_GB, OS, DOMAIN, LOCATION, BUILDING, ROOM, MACADDR, PURCHASE_DATE, WARRANTY_END) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                $checkSerialQuery = "SELECT COUNT(*) FROM ordinateur WHERE NAME = ?";
+            // Vérification que les colonnes requises existent dans l'en-tête
+            $columnsFound = array_intersect($header, $requiredColumns);
+            if (count($columnsFound) != count($requiredColumns)) {
+                header("Location: ../ajoutCSVMachines.php?error=missing_columns");
+                exit();
+            }
 
-                if (!$connect) {
-                    die("Connection failed: " . mysqli_connect_error());
-                }
+            // Préparation de la connexion
+            if (!$connect) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
 
-                $action = "Import d'ordinateurs via CSV";
+            if ($stmt = mysqli_prepare($connect, $insertQuery)) {
+                if ($checkStmt = mysqli_prepare($connect, $checkSerialQuery)) {
+                    $errorOccurred = false;
 
-                if ($stmt = mysqli_prepare($connect, $insert)) {
-                    if ($checkStmt = mysqli_prepare($connect, $checkSerialQuery)) {
-                        $errorOccurred = false;
+                    while (($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
+                        // Mapper les données CSV avec les noms de colonnes
+                        $dataMap = array_combine($header, $data);
 
-                        while (($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
-                            if (count($data) == 16) {
-                                mysqli_stmt_bind_param($checkStmt, 's', $data[0]);
+                        // Remplacer les valeurs vides par NULL
+                        if ($_POST['type_csv'] == "moniteurs") {
+                            $serial = !empty($dataMap['SERIAL']) ? $dataMap['SERIAL'] : NULL;
+                            $manufacturer = !empty($dataMap['MANUFACTURER']) ? $dataMap['MANUFACTURER'] : NULL;
+                            $model = !empty($dataMap['MODEL']) ? $dataMap['MODEL'] : NULL;
+                            $size_inch = !empty($dataMap['SIZE_INCH']) ? $dataMap['SIZE_INCH'] : NULL;
+                            $resolution = !empty($dataMap['RESOLUTION']) ? $dataMap['RESOLUTION'] : NULL;
+                            $connector = !empty($dataMap['CONNECTOR']) ? $dataMap['CONNECTOR'] : NULL;
+                            $attached_to = !empty($dataMap['ATTACHED_TO']) ? $dataMap['ATTACHED_TO'] : NULL;
 
-                                mysqli_stmt_execute($checkStmt);
+                            // Vérification de la présence du SERIAL
+                            mysqli_stmt_bind_param($checkStmt, 's', $serial);
+                        } elseif ($_POST['type_csv'] == "ordinateurs") {
+                            $name = !empty($dataMap['NAME']) ? $dataMap['NAME'] : NULL;
+                            $serial = !empty($dataMap['SERIAL']) ? $dataMap['SERIAL'] : NULL;
+                            $manufacturer = !empty($dataMap['MANUFACTURER']) ? $dataMap['MANUFACTURER'] : NULL;
+                            $model = !empty($dataMap['MODEL']) ? $dataMap['MODEL'] : NULL;
+                            $type = !empty($dataMap['TYPE']) ? $dataMap['TYPE'] : NULL;
+                            $cpu = !empty($dataMap['CPU']) ? $dataMap['CPU'] : NULL;
+                            $ram_mb = !empty($dataMap['RAM_MB']) ? $dataMap['RAM_MB'] : NULL;
+                            $disk_gb = !empty($dataMap['DISK_GB']) ? $dataMap['DISK_GB'] : NULL;
+                            $os = !empty($dataMap['OS']) ? $dataMap['OS'] : NULL;
+                            $domain = !empty($dataMap['DOMAIN']) ? $dataMap['DOMAIN'] : NULL;
+                            $location = !empty($dataMap['LOCATION']) ? $dataMap['LOCATION'] : NULL;
+                            $building = !empty($dataMap['BUILDING']) ? $dataMap['BUILDING'] : NULL;
+                            $room = !empty($dataMap['ROOM']) ? $dataMap['ROOM'] : NULL;
+                            $macaddr = !empty($dataMap['MACADDR']) ? $dataMap['MACADDR'] : NULL;
+                            $purchase_date = !empty($dataMap['PURCHASE_DATE']) ? $dataMap['PURCHASE_DATE'] : NULL;
+                            $warranty_end = !empty($dataMap['WARRANTY_END']) ? $dataMap['WARRANTY_END'] : NULL;
 
-                                mysqli_stmt_bind_result($checkStmt, $count);
-                                mysqli_stmt_fetch($checkStmt);
+                            // Vérification de la présence du NAME
+                            mysqli_stmt_bind_param($checkStmt, 's', $name);
+                        }
 
-                                mysqli_stmt_free_result($checkStmt);
+                        mysqli_stmt_execute($checkStmt);
+                        mysqli_stmt_bind_result($checkStmt, $count);
+                        mysqli_stmt_fetch($checkStmt);
+                        mysqli_stmt_free_result($checkStmt);
 
-                                if ($count > 0) {
-                                    $errorOccurred = true;
-                                } else {
-                                    mysqli_stmt_bind_param($stmt, 'ssssssiissssssss', $data[0], $data[1], $data[2], $data[3], $data[4], $data[5], $data[6],$data[7],$data[8],$data[9],$data[10],$data[11],$data[12],$data[13],$data[14],$data[15]);
-                                    mysqli_stmt_execute($stmt);
-                                }
-                            } else {
-                                header("Location: ../ajoutCSVMachines.php?error=invalid_columns");
-                                exit();
+                        if ($count > 0) {
+                            $errorOccurred = true;
+                        } else {
+                            // Insertion des données dans la base
+                            if ($_POST['type_csv'] == "moniteurs") {
+                                mysqli_stmt_bind_param($stmt, 'sssissss', $serial, $manufacturer, $model, $size_inch, $resolution, $connector, $attached_to,$statut);
+                            } elseif ($_POST['type_csv'] == "ordinateurs") {
+                                mysqli_stmt_bind_param($stmt, 'ssssssiisssssssss', $name, $serial, $manufacturer, $model, $type, $cpu, $ram_mb, $disk_gb, $os, $domain, $location, $building, $room, $macaddr, $purchase_date, $warranty_end,$statut);
                             }
+                            mysqli_stmt_execute($stmt);
                         }
-
-                        if ($errorOccurred) {
-                            header("Location: ../ajoutCSVMachines.php?error=serial_exists");
-                            exit();
-                        }
-
-                        insertionLog($action);
-                        header("Location: ../ajoutCSVMachines.php?sucess");
-                        exit();
-                    } else {
-                        die('Error preparing check serial query: ' . mysqli_error($connect));
                     }
+
+                    if ($errorOccurred) {
+                        header("Location: ../ajoutCSVMachines.php?error=name_exists");
+                        exit();
+                    }
+
+                    insertionLog($action);
+                    header("Location: ../ajoutCSVMachines.php?success");
+                    exit();
                 } else {
-                    die('Error preparing insert query: ' . mysqli_error($connect));
+                    die('Error preparing check serial query: ' . mysqli_error($connect));
                 }
+            } else {
+                die('Error preparing insert query: ' . mysqli_error($connect));
             }
 
             fclose($handle);
--- src/site/pages/ajoutCSVMachines.php
+++ src/site/pages/ajoutCSVMachines.php
@@ -11,26 +11,49 @@ include("../fragment/navbarTech.php");
             <input type="file" name="csv_file" id="csv_file" accept=".csv" required>
             <label>Selectionner le type du CSV:</label>
             <select name="type_csv">
-                <option value="moniteurs">Moniteurs</option>
-                <option value="ordinateurs">Ordinateurs</option>
+                <option value="moniteurs">Moniteurs (numéro de série obligatoire)</option>
+                <option value="ordinateurs">Ordinateurs (nom et numéro série obligatoire)</option>
             </select>
             <button type="submit" name="submit">Importer</button>
         </form>
         <?php
         if (isset($_GET['error'])){
-            if ($_GET['error']=="invalid_columns"){
+            if ($_GET['error']=="invalid_type"){
                 echo"<div class='connexion-error'>
-                    Nombre de colonnes du fichier invalide
+                    Type du fichier invalide
                 </div>";
             }
-            if ($_GET['error']=="serial_exists"){
+            if ($_GET['error']=="missing_columns"){
                 echo"<div class='connexion-error'>
-                    Numéro de série deja existant
+                    Colonnes manquantes
+                </div>";
+            }
+            if ($_GET['error']=="name_exists"){
+                echo"<div class='connexion-error'>
+                    Numéro de série ou Nom déja existant
+                </div>";
+            }
+
+            if ($_GET['error']=="file_open"){
+                echo"<div class='connexion-error'>
+                    Erreur lors de l'ouverture du fichier
+                </div>";
+            }
+
+            if ($_GET['error']=="file_upload"){
+                echo"<div class='connexion-error'>
+                    Erreur lors du téléversement du fichier'
+                </div>";
+            }
+
+            if ($_GET['error']=="form_submit"){
+                echo"<div class='connexion-error'>
+                    Erreur lors de la soumission du formulaire
                 </div>";
             }
 
         }
-        if (isset($_GET['sucess'])){
+        if (isset($_GET['success'])){
             echo"<div class='connexion-success'>
                     Ajout(s) effectué(s)
                 </div>";
